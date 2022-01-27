ccf9762a9b9a9344a3c9f8040af8cb53131d8425
emmanuelflambard <emmanuel.flambard@gmail.com>
2022-01-27 12:58:57 -0500

    commit4

---
+++ server/routes/Comments.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { Comments } = require("../models");
+
+router.get("/", async (req, res) => {
+  const listOfComments = await Comments.findAll();
+  res.json(listOfComments);
+});
+
+router.post("/", async (req, res) => {
+  const comment = req.body;
+  await Comments.create(comment);
+  res.json(comment);
+});
+
+module.exports = router;
